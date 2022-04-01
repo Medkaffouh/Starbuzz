4d7df864a7eb7f2eb45b22711fd9f5d186643721
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-01 21:45:40 +0000

    pass an id with the intent and start a new activity

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -3,6 +3,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
 public class DrinkCategoryActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,22 @@ public class DrinkCategoryActivity extends Activity {
                 Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        //Pass the drink the user clicks on to DrinkActivity
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
